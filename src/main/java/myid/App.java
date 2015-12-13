1031670207c3de6c8d6b8527d766327d168f6485
Vlad <vlad.gryadovkin@yandex.by>
2015-12-14 02:17:26 +0300

    with new branch

--- src/main/java/myid/App.java
+++ src/main/java/myid/App.java
@@ -1,13 +1,18 @@
 package myid;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        System.out.println( str );
     }
 }
